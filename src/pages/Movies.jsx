a770b67ca43e744b0ec12aceabc56098da2eb5d5
David Rajcher <david.rajcher@gmail.com>
2026-01-30 17:53:28 +0100

    removed movies

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,77 +1,59 @@
-import marioMovie from '../assets/movies/mario.webp'
-import paddingtonMovie from '../assets/movies/paddington.jpg'
-import spidermanMovie from '../assets/movies/spiderman.jpg'
-import legoMovie from '../assets/movies/lego.webp'
-import rutgerThomas from '../assets/movies/rutger_thomas.jpg'
-import minecraftMovie from '../assets/movies/minecraft.jpg'
-import jurassicPark from '../assets/movies/jurassic park.jpg'
-import { useLanguage } from '../i18n/LanguageContext.jsx'
+import marioMovie from "../assets/movies/mario.webp";
+import paddingtonMovie from "../assets/movies/paddington.jpg";
+import spidermanMovie from "../assets/movies/spiderman.jpg";
+import legoMovie from "../assets/movies/lego.webp";
+import rutgerThomas from "../assets/movies/rutger_thomas.jpg";
+import minecraftMovie from "../assets/movies/minecraft.jpg";
+import jurassicPark from "../assets/movies/jurassic park.jpg";
+import { useLanguage } from "../i18n/LanguageContext.jsx";
 
 function Movies() {
-  const { copy } = useLanguage()
+  const { copy } = useLanguage();
   const movies = [
     {
-      title: 'The Super Mario Bros. Movie',
-      text: copy.movies.items.mario,
-      image: marioMovie,
-      url: 'https://www.imdb.com/title/tt6718170/',
-    },
-    {
-      title: 'Paddington',
-      text: copy.movies.items.paddington,
-      image: paddingtonMovie,
-      url: 'https://www.imdb.com/title/tt1109624/',
-    },
-    {
-      title: 'Spider-Man: Into the Spider-Verse',
-      text: copy.movies.items.spider,
-      image: spidermanMovie,
-      url: 'https://www.imdb.com/title/tt4633694/',
-    },
-    {
-      title: 'The LEGO Movie',
+      title: "The LEGO Movie",
       text: copy.movies.items.lego,
       image: legoMovie,
-      url: 'https://www.imdb.com/title/tt1490017/',
+      url: "https://www.imdb.com/title/tt1490017/",
     },
     {
-      title: 'Rutger, Thomas and Paco',
+      title: "Rutger, Thomas and Paco",
       text: copy.movies.items.rutger,
       image: rutgerThomas,
-      url: 'https://www.imdb.com/title/tt36650010/',
+      url: "https://www.imdb.com/title/tt36650010/",
     },
     {
-      title: 'Minecraft Movie',
+      title: "Minecraft Movie",
       text: copy.movies.items.minecraft,
       image: minecraftMovie,
-      url: 'https://www.imdb.com/title/tt3566834/',
+      url: "https://www.imdb.com/title/tt3566834/",
     },
     {
-      title: 'Jurassic Park',
+      title: "Jurassic Park",
       text: copy.movies.items.jurassic,
       image: jurassicPark,
-      url: 'https://www.imdb.com/title/tt0107290/',
+      url: "https://www.imdb.com/title/tt0107290/",
     },
-  ]
+  ];
 
   return (
-    <div className="page">
-      <div className="page-header">
+    <div className='page'>
+      <div className='page-header'>
         <h1>{copy.movies.title}</h1>
         <p>{copy.movies.intro}</p>
       </div>
 
-      <section className="grid">
+      <section className='grid'>
         {movies.map((movie) => (
           <a
             key={movie.title}
-            className="card youtube-card"
+            className='card youtube-card'
             href={movie.url}
-            target="_blank"
-            rel="noreferrer"
+            target='_blank'
+            rel='noreferrer'
           >
             <img
-              className="card-image youtube-image"
+              className='card-image youtube-image'
               src={movie.image}
               alt={movie.title}
             />
@@ -81,7 +63,7 @@ function Movies() {
         ))}
       </section>
     </div>
-  )
+  );
 }
 
-export default Movies
+export default Movies;
